27e06aa48540a96ae46f658097cf9607469ec17c
Nicolas Modrzyk <hellonico@gmail.com>
2012-11-30 15:14:28 +0900

    test for one file output

---
+++ test/medic/test/one.clj
@@ -0,0 +1,20 @@
+(ns medic.test.one
+  (:require medic.pdf)
+  (:use midje.sweet)
+  (:use [clojure.java.io :as io])
+  (:use medic.core)
+  (:use medic.modify)
+  (:use medic.parse)
+  (:use clojure.test)
+  (:use jsoup.soup))
+
+(deftest one-file
+	(set-options 
+		{
+		 :customization "public/html"
+		 :folder "../niclojure/textja"
+		 :toc-filename "toc.html"
+		 :one true
+		 :output "output"})
+	(toc-folder "../niclojure/textja")
+	(is (.exists (io/as-file "output/one.html")) "One file does not exist"))
