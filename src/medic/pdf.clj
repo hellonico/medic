5826504d87ec1c048fce24ada77835ec5092354c
Nicolas Modrzyk <hellonico@gmail.com>
2012-12-03 14:00:54 +0900

    fixing PDF generation problem regarding CSS files

--- src/medic/pdf.clj
+++ src/medic/pdf.clj
@@ -3,11 +3,36 @@
   (:import [java.io FileOutputStream])
   (:import [org.xhtmlrenderer.pdf ITextRenderer]))
 
+(defn- get-factory
+  "Get me an old fashion sax parser factory"
+  []
+    (doto (javax.xml.parsers.DocumentBuilderFactory/newInstance)
+      (.setValidating false)
+      (.setFeature "http://apache.org/xml/features/nonvalidating/load-dtd-grammar" false)
+      (.setFeature "http://apache.org/xml/features/nonvalidating/load-external-dtd" false)
+      (.setFeature "http://xml.org/sax/features/validation" false)))
+(def factory (get-factory))
+
+(defn- get-doc
+  "This parse the document with the proper sax properties"
+  [filepath]
+    (.parse (.newDocumentBuilder factory) (as-file filepath)))
+
+(defn- get-parent
+  "Needed so that CSS files are resolved properly"
+  [filepath]
+  (-> 
+    (as-file filepath)
+    (.getParentFile)
+    (.toURI)
+    (.toURL)
+    (.toExternalForm)))
+
 (defn- add-file-to-pdf
   "Cannot use this on its own. This is called by generate-pdf to add more files."
-  [renderer document]
+  [renderer filepath]
      (doto renderer
-      (.setDocument document)
+      (.setDocument (get-doc filepath) (get-parent filepath))
       (.layout)
       (.writeNextDocument)))
 
@@ -19,9 +44,9 @@
       ; the first file is handled slightly differently by flying saucer
       ; it handles the style and the layout
       (doto renderer 
-        (.setDocument (as-file first-file))
+        (.setDocument (get-doc first-file) (get-parent first-file))
         (.layout)
         (.createPDF os false))
       (doseq [file (remove #{first-file} in-files)]
-        (add-file-to-pdf renderer (as-file file)))
+        (add-file-to-pdf renderer file))
       (.finishPDF renderer))))
