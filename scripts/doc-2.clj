628e34363bf1e3d14b1d246b2f49ce40275cbcd6
Nicolas Modrzyk <nico@fivecool.net>
2012-12-04 15:42:56 +0900

    docs on separate files

---
+++ scripts/doc-2.clj
@@ -0,0 +1,9 @@
+(use 'medic.core) 
+(medic.core/toc-with-options 
+	{:folder "doc" 
+	 :customization "doc"  
+	 :output "output" 
+	 :one false 
+	 :clean true 
+	 :embed false
+	})
---
+++ scripts/generate-doc-with-jark-cols.sh
@@ -0,0 +1,2 @@
+cat scripts/doc-2.clj | jark -e 
+cp public/html/frameset.html output
